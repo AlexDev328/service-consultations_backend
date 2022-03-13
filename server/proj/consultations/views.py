00f20c221fe500f208361c568194ff88bed2e615
AIDev <developer328555@gmail.com>
2022-03-14 01:46:33 +0400

    load backup project ref

--- server/proj/consultations/views.py
+++ server/proj/consultations/views.py
@@ -4,22 +4,21 @@ import secrets
 from random import choice
 from PIL import Image
 from django.db.models import Q
-from django.core.exceptions import *
 from django.conf import settings
 from django.core.files.base import ContentFile
 from rest_framework import status, mixins
 from rest_framework.decorators import api_view
-
-
-
+from string import ascii_lowercase, digits
 from rest_framework.generics import (ListCreateAPIView, RetrieveUpdateDestroyAPIView, ListAPIView, )
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
+
 from .forms import UploadFileForm
 from .models import UserProfile, User, Photo, Consultation, AuthCode
 from .permissions import IsOwnerProfileOrReadOnly
 from .serializers import UserProfileSerializer, ConsultationSerializer
+from .token_generator import get_tokens_for_user
 
 
 class UserProfileListCreateView(ListCreateAPIView):
@@ -47,9 +46,6 @@ class UserProfileDetailView(RetrieveUpdateDestroyAPIView):
             super().retrieve(request, *args, **kwargs)
 
 
-
-
-
 class ConsultationView(ModelViewSet):
     permission_classes_by_action = {'retrieve': [AllowAny],
                                     'patch': [AllowAny]}
@@ -61,8 +57,6 @@ class ConsultationView(ModelViewSet):
             return Consultation.objects.filter(Q(consultant__user=self.request.user) | Q(status=0))
         else:
             return Consultation.objects.all()
-
-
 
 
 def upload_file(request):
@@ -106,37 +100,7 @@ def get_dataurl_from_image(file, resize, base_width=300):
     with open(file, "rb") as image_file:
         encoded_string = base64.b64encode(image_file.read())
         print(encoded_string)
-        return ('data:image/png;base64,' + str(encoded_string)[2:-1])
-    return ''
-
-
-'''def upload_conclusion(request):
-    if request.method == 'POST':
-        app_id = request.POST.get('application_id')
-        cons_text = request.POST.get('cons_text')
-        final = request.POST.get('final')
-
-        if final == "True":
-            final = True
-        else:
-            final = False
-        try:
-            conclusion = Conclusion.objects.get(application=Application.objects.get(pk=app_id))
-            conclusion.cons_text = cons_text
-            conclusion.final = final
-        except ObjectDoesNotExist:
-            conclusion = Conclusion.objects.create(application=Application.objects.get(pk=app_id), cons_text=cons_text,
-                                                   final=final)
-        parsed_images = json.loads(request.POST.get("imgs"))
-        conclusion.save()
-        # не загружаем уже загруженные изображения( т.к. они у нс не удаляются на фронте, необходимо отсеивать уже существующие)
-        img_count = Photo.objects.filter(conclusion=conclusion).count()
-        for i in range(img_count, len(parsed_images)):
-            instance = Photo(img=get_image_from_data_url(parsed_images[i], False), conclusion_id=conclusion.pk)
-            instance.save()
-    return HttpResponse(status=status.HTTP_201_CREATED)'''
-
-from string import ascii_lowercase, digits
+        return 'data:image/png;base64,' + str(encoded_string)[2:-1]
 
 
 def generate_random_username(length=16, chars=ascii_lowercase + digits, split=4, delimiter='-'):
@@ -149,9 +113,6 @@ def generate_random_username(length=16, chars=ascii_lowercase + digits, split=4,
         return generate_random_username(length=length, chars=chars, split=split, delimiter=delimiter)
     except User.DoesNotExist:
         return username
-
-
-from .token_generator import get_tokens_for_user
 
 
 @api_view(['POST'])
@@ -171,7 +132,8 @@ def create_application(request):
             user.set_password(password)
             user.save()
             auth_code = AuthCode.objects.create()
-            current_user_profile = UserProfile.objects.create(user=user, user_inner_id=userdata['id'], auth_code=auth_code)
+            current_user_profile = UserProfile.objects.create(user=user, user_inner_id=userdata['id'],
+                                                              auth_code=auth_code)
             current_user_profile.save()
 
         res = Consultation.objects.create(insigator=current_user_profile, data=data['params'])
@@ -192,7 +154,6 @@ def aprove_application(request, id):
         consultation.save()
         return Response(status=status.HTTP_200_OK, data=ConsultationSerializer(consultation).data)
     return Response(status=status.HTTP_208_ALREADY_REPORTED)
-
 
 
 @api_view(['GET'])
